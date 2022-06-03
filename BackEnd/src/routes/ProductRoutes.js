212ff2d7d400df759662f44d1256bdfb9f446926
Alvaro Santisteban <asantisteban@mandu.pe>
2022-06-02 19:53:10 -0500

    products & users routes

--- BackEnd/src/routes/ProductRoutes.js
+++ BackEnd/src/routes/ProductRoutes.js
@@ -1,4 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 
+router.route('/')
+    .get((req, res) => res.json({ message: 'GET products' }))
+    .post((req, res) => res.json({ message: 'POST products' }))
+
 module.exports = router;
--- BackEnd/src/routes/UserRoutes.js
+++ BackEnd/src/routes/UserRoutes.js
@@ -1,4 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 
+router.route('/')
+    .get((req, res) => res.json({ message: 'GET users' }))
+    .post((req, res) => res.json({ message: 'POST users' }))
+
 module.exports = router;
