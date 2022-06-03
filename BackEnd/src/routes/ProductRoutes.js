c9f7406e0a9c2a67d9e81e9d542a5acfaa770626
Alvaro Santisteban <asantisteban@mandu.pe>
2022-06-02 20:03:36 -0500

    update routes iter 3

--- BackEnd/src/routes/ProductRoutes.js
+++ BackEnd/src/routes/ProductRoutes.js
@@ -5,4 +5,9 @@ router.route('/')
     .get((req, res) => res.json({ message: 'GET products' }))
     .post((req, res) => res.json({ message: 'POST products' }))
 
+router.route('/:id')
+    .get((req, res) => res.json({ tittle: 'tittle product' }))
+    .put((req, res) => res.json({ message: 'product updated' }))
+    .delete((req, res) => res.json({ message: 'product delete' }))
+
 module.exports = router;
--- BackEnd/src/routes/UserRoutes.js
+++ BackEnd/src/routes/UserRoutes.js
@@ -5,4 +5,7 @@ router.route('/')
     .get((req, res) => res.json({ message: 'GET users' }))
     .post((req, res) => res.json({ message: 'POST users' }))
 
+router.route('/:id')
+    .delete((req, res) => res.json({ message: 'user delete' }))
+
 module.exports = router;
